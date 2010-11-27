f67559c872e42a56d77268bf477bcd218e2b3889
Bryce Kerley <bkerley@brycekerley.net>
2010-11-27 15:56:20 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triskelion::Application.config.secret_token = 'badece6b6299c608aa52b082a5599a8af7a688a366bbb7c9ce43af420802dbc69a172189d73943ef9e3ef616cd84e0ce3ff52106ab94effa740bf3ad37885298'
