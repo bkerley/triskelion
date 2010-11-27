a4096f5d0957f2501e07b701dabd1a7a04bf7e1d
Bryce Kerley <bkerley@brycekerley.net>
2010-11-27 18:42:11 -0500

    basic chattin'

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,14 @@
 class ChatsController < ApplicationController
+  before_filter :load_party
   def index
+    @chats = @party.chats.all :order=>'created_at desc', :limit=>50
   end
 
   def new
   end
 
+  def create
+    @chat = @party.chats.create! :player=>current_player, :content=>params[:chat][:content]
+    redirect_to party_chats_path @party.id
+  end
 end
