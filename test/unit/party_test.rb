b33291a5ffc17a20136eac838726f7e954fbbc60
Bryce Kerley <bkerley@brycekerley.net>
2010-11-27 16:23:51 -0500

    association tests for party

--- test/unit/party_test.rb
+++ test/unit/party_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class PartyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'a Party' do
+    setup do
+      @party = Factory :party
+    end
+
+    should have_many :rolls
+    should have_many :chats
   end
 end
