c2498887cd15f0ca8ea1d968e8f6099f1265d9a6
Bryce Kerley <bkerley@brycekerley.net>
2010-11-27 19:14:28 -0500

    rolls controller tests pass

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,8 +1,14 @@
 class RollsController < ApplicationController
+  before_filter :load_party
   def index
+    @rolls = @party.rolls.all :order=>'created_at desc', :limit=>50
   end
 
   def new
   end
 
+  def create
+    @roll = Roll.create_with_code :code=>params[:roll][:code], :player=>current_player, :party=>@party
+    redirect_to party_rolls_path @party.id
+  end
 end
