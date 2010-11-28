81086fb5a79b245ea3945abef809e6f0c2bceffe
Bryce Kerley <bkerley@brycekerley.net>
2010-11-27 20:45:09 -0500

    implement tripcode digest

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ActiveRecord::Base
   has_many :rolls
   has_many :chats
+
+  def tripcode_digest
+    [Digest::SHA1.digest(self.tripcode)].pack('m').tr('+/','-_')[0..7]
+  end
 end
--- test/unit/player_test.rb
+++ test/unit/player_test.rb
@@ -8,5 +8,9 @@ class PlayerTest < ActiveSupport::TestCase
 
     should have_many :rolls
     should have_many :chats
+
+    should 'have a tripcode_digest' do
+      assert @player.tripcode_digest
+    end
   end
 end
