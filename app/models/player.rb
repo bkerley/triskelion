c95ef3d0463e9c065b9eac4b13f532a92c30d484
Robert Surrency <garnth@may.(none)>
2010-12-26 21:25:43 -0500

    More stylin, random name colors 0.1

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,15 @@ class Player < ActiveRecord::Base
   def tripcode_digest
     [Digest::SHA1.digest(self.tripcode)].pack('m').tr('+/','-_')[0..7]
   end
+
+  def colorcode_digest
+    Digest::SHA1.hexdigest(self.tripcode+self.name)
+  end
+
+  def chat_color
+    ccode = "#9" + colorcode_digest[0,3][0]
+    ccode += "9" + colorcode_digest[0,3][1]
+    ccode += "9" + colorcode_digest[0,3][2]
+    return ccode;
+  end
 end
