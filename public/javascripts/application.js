4afda238ed858f5f4826bd45507007d9fabf2696
Robert Surrency <garnth@may.(none)>
2010-12-27 22:31:39 -0500

    More Stylin, also some javascript to set focus automatically

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
 function scroll_to_bottom(el_id){
     var el = document.getElementById(el_id);
     if(el){
         el.scrollTop = el.scrollHeight;
+    }
+}
+
+function set_focus(el_id){
+    var el = document.getElementById(el_id);
+    if(el){
+        el.focus();
     }
 }
