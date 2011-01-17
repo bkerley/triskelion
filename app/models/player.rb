2d59250d6197e447e36b71e40cd8182fe445ab52
Bryce Kerley <bkerley@brycekerley.net>
2011-01-17 14:38:07 -0500

    tripcode digest now includes name

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@ class Player < ActiveRecord::Base
   has_many :chats
 
   def tripcode_digest
-    [Digest::SHA1.digest(self.tripcode)].pack('m').tr('+/','-_')[0..7]
+    [Digest::SHA1.digest(self.tripcode+self.name)].pack('m').tr('+/','-_')[0..7]
   end
 
   def colorcode_digest
